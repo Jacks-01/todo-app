8900367a54193fb9c3986a5912bcf7c954168131
Jack Stubblefield <Jack011601@gmail.com>
2022-11-01 12:48:03 -0500

    added pagination

--- src/Components/List/index.jsx
+++ src/Components/List/index.jsx
@@ -1,27 +1,42 @@
-import { useContext, useState } from "react";
-import {SettingsContext} from '../../Context/Settings/index';
+import { Pagination } from '@mantine/core';
+import { useContext, useState } from 'react';
+import { When } from 'react-if';
+import { SettingsContext } from '../../Context/Settings/index';
 
 const List = ({ list, toggleComplete }) => {
+	const { pageItems, showCompleted } = useContext(SettingsContext);
+	const [page, setPage] = useState(1);
+	// pagination
+	const listToRender = showCompleted
+		? list
+		: list.filter((item) => !item.complete);
+	const listStart = pageItems * (page - 1);
+	const listEnd = listStart + pageItems;
+	const pageCount = Math.ceil(listToRender.length / pageItems);
+	const displayList = listToRender.slice(listStart, listEnd);
+	return (
+		<>
+            {displayList.map(item => (
+                <div key={item.id}>
+                    <p>{item.text}</p>
+                    <p>
+                        <small>Assigned to: {item.assignee}</small>
+                    </p>
+                    <p>
+                        <small>Difficulty: {item.difficulty}</small>
+                    </p>
+                    <div onClick={() => toggleComplete(item.id)}>
+                        Complete: {item.complete.toString()}
+                    </div>
+                    <hr />
+                </div>
+            ))}
 
-    const { pageItems, showCompleted } = useContext(SettingsContext);
-    const [page, setPage] = useState(1);
-    // pagination
-    const listToRender = showCompleted ? list : list.filter(item => !item.complete);
-    const listStart = pageItems * (page - 1)
-    const listEnd = listStart + pageItems;
-    const pageCount = Math.ceil(listToRender.length / pageItems);
-    const displayList = listToRender.slice(listStart, listEnd);
-    return ( 
-        list.map(item => (
-            <div key={item.id}>
-              <p>{item.text}</p>
-              <p><small>Assigned to: {item.assignee}</small></p>
-              <p><small>Difficulty: {item.difficulty}</small></p>
-              <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-              <hr />
-            </div>
-          ))
-     );
-}
- 
+            <When condition={listToRender.length > 0}>
+                <Pagination page={page} onChange={setPage} total={pageCount}/>
+            </When>
+		</>
+	);
+};
+
 export default List;
