89ae664389599292579055a3c3ec8ab23af665e0
Jack Stubblefield <Jack011601@gmail.com>
2022-10-31 19:30:04 -0500

    created file scaffolding

--- src/Components/ToDo/index.js
+++ src/Components/ToDo/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import useForm from '../../hooks/form.js';
+import useForm from '../src/hooks/form.js';
 
 import { v4 as uuid } from 'uuid';
 
@@ -20,15 +20,15 @@ const ToDo = () => {
   }
 
   function deleteItem(id) {
-    const items = list.filter( item => item.id !== id );
+    const items = list.filter(item => item.id !== id);
     setList(items);
   }
 
   function toggleComplete(id) {
 
-    const items = list.map( item => {
-      if ( item.id === id ) {
-        item.complete = ! item.complete;
+    const items = list.map(item => {
+      if (item.id === id) {
+        item.complete = !item.complete;
       }
       return item;
     });
@@ -44,7 +44,7 @@ const ToDo = () => {
     // linter will want 'incomplete' added to dependency array unnecessarily. 
     // disable code used to avoid linter warning 
     // eslint-disable-next-line react-hooks/exhaustive-deps 
-  }, [list]);  
+  }, [list]);
 
   return (
     <>
