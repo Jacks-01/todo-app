0587de761ab0306f8fc7627af5463663cc2c040a
Jack Stubblefield <jack011601@gmail.com>
2022-11-06 15:27:37 -0600

    read access enforced

--- src/Components/List/index.jsx
+++ src/Components/List/index.jsx
@@ -35,49 +35,51 @@ const List = ({ deleteItem, list, toggleComplete }) => {
 	const { classes } = useStyles();
 	return (
 		<>
-			{displayList.map((item) => (
-				<Card
-					key={item.id}
-					shadow='md'
-					p='lg'
-					radius='md'
-					withBorder
-				>
-					<Card.Section withBorder>
-						<Group>
-							<Badge
-								className={classes.badge}
-								color={item.complete ? 'green' : 'red'}
-								variant='filled'
-								onClick={() => toggleComplete(item.id)}
-							>
-								{item.complete ? 'complete' : 'pending'}
-							</Badge>
-							<Text>{item.assignee}</Text>
-							<Auth capability='delete'>
-								<CloseButton
-									title='Delete ToDo Item'
-									onClick={() => deleteItem(item.id)}
-								></CloseButton>
-							</Auth>
+			<Auth capability='read'>
+				{displayList.map((item) => (
+					<Card
+						key={item.id}
+						shadow='md'
+						p='lg'
+						radius='md'
+						withBorder
+					>
+						<Card.Section withBorder>
+							<Group>
+								<Badge
+									className={classes.badge}
+									color={item.complete ? 'green' : 'red'}
+									variant='filled'
+									onClick={() => toggleComplete(item.id)}
+								>
+									{item.complete ? 'complete' : 'pending'}
+								</Badge>
+								<Text>{item.assignee}</Text>
+								<Auth capability='delete'>
+									<CloseButton
+										title='Delete ToDo Item'
+										onClick={() => deleteItem(item.id)}
+									></CloseButton>
+								</Auth>
+							</Group>
+						</Card.Section>
+
+						<Text>{item.text}</Text>
+
+						<Group align='right'>
+							<Text>Difficulty: {item.difficulty}</Text>
 						</Group>
-					</Card.Section>
+					</Card>
+				))}
 
-					<Text>{item.text}</Text>
-
-					<Group align='right'>
-						<Text>Difficulty: {item.difficulty}</Text>
-					</Group>
-				</Card>
-			))}
-
-			<When condition={listToRender.length > 0}>
-				<Pagination
-					page={page}
-					onChange={setPage}
-					total={pageCount}
-				/>
-			</When>
+				<When condition={listToRender.length > 0}>
+					<Pagination
+						page={page}
+						onChange={setPage}
+						total={pageCount}
+					/>
+				</When>
+			</Auth>
 		</>
 	);
 };
