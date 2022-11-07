09f6bf45d4b16e6cb772278a46733fa971ed69b0
Jack Stubblefield <Jack011601@gmail.com>
2022-11-07 11:23:45 -0600

    added header to settings page

--- src/routes/Settings/Settings.jsx
+++ src/routes/Settings/Settings.jsx
@@ -1,36 +1,50 @@
+import { useContext } from 'react';
 import { useForm } from '@mantine/form';
 import AppHeader from '../../Components/Header';
 import { Container, createStyles, Text } from '@mantine/core';
-import { AuthContext } from '../../Context/Auth';
 import { SettingsContext } from '../../Context/Settings';
+import { IconSettings } from '@tabler/icons';
 
 export const useStyles = createStyles((theme) => ({
 	container: {
 		width: '80vw',
 		marginTop: '3rem',
 	},
+	header: {
+		backgroundColor: theme.colors.dark[5],
+		color: 'whitesmoke',
+		height: '3rem',
+		borderRadius: theme.radius.sm,
+		display: 'flex',
+		alignItems: 'center',
+		padding: '1rem',
+		size: theme.fontSizes.md,
+	},
 }));
 
 const Settings = ({}) => {
 	const form = useForm({
-		initialValues: {
-			username: '',
-			password: '',
-		},
+		initialValues: {},
 	});
 
 	const { classes } = useStyles();
+
+	const {
+		showCompleted,
+		pageItems,
+		sort,
+		setShowCompleted,
+		setPageItems,
+		setSort,
+	} = useContext(SettingsContext);
 
 	return (
 		<>
 			<AppHeader />
 			<Container className={classes.container}>
-				<header data-testid='todo-header'>
-					<Text
-						className={classes.header}
-						data-testid='todo-h1'
-					>
-						To Do List: {incomplete} items pending
+				<header>
+					<Text className={classes.header}>
+						<IconSettings style={{marginRight: '.5em'}}/> Settings
 					</Text>
 				</header>
 			</Container>
